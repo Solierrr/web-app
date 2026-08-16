67d6c12e14a54dcf136cf51d5802d8d18a9c494f
meguito <doasilva2009@hotmail.com>
2026-08-16 20:17:15 -0300

    style: universe reset and text classes

---
+++ src/domain/enum/colors.ts
@@ -0,0 +1,13 @@
+enum Colors {
+    White = "var(--color-white)",
+    Gray  = "var(--color-gray)",
+    Black = "var(--color-black)",
+
+    Orange = "var(--color-orange)",
+    Green  = "var(--color-green)",
+    Input  = "var(--color-input)",
+
+    HyperBlue = "var(--color-hyperlinkblue)",
+}
+
+export default Colors;
