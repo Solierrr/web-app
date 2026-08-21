791127488fada844b1a09fe1d18f2dfa1639736e
meguito <doasilva2009@hotmail.com>
2026-08-21 07:48:24 -0300

    feat: setup project domain enum

---
+++ src/domain/enum/companyStatus.ts
@@ -0,0 +1,7 @@
+enum CompanyStatus {
+    Approved      = "APPROVED",
+    Rejected      = "REJECTED",
+    UnderAnalysis = "UNDER_ANALYSIS",
+}
+
+export default CompanyStatus;
---
+++ src/domain/errors/InvalidIconError.ts
@@ -0,0 +1,10 @@
+export class InvalidIconError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = "InvalidIconError";
+    }
+
+    static notFound(name: string): InvalidIconError {
+        return new InvalidIconError(`Icon: o ícone \`${name}\` não foi encontrado.`);
+    }
+}
