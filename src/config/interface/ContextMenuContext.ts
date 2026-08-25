6d95bcd75c272f68562e92e450f9f2535d54d314
meguito <doasilva2009@hotmail.com>
2026-08-25 08:15:50 -0300

    del: refactor components architeture

--- src/config/interface/ContextMenuContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import type { ContextMenuItem } from "@/config/interface/ContextMenu";
-
-export interface ContextMenuContextValue {
-  open:  (items: ContextMenuItem[], x: number, y: number) => void;
-  close: () => void;
-}
-
-export const ContextMenuContext = createContext<ContextMenuContextValue | null>(null);
