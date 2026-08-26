582cd0293d51166b1c669eebe60108e6525b5702
meguito <doasilva2009@hotmail.com>
2026-08-25 22:04:45 -0300

    fix: component alias

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
+      '@@': fileURLToPath(new URL('./src/components', import.meta.url)),
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
