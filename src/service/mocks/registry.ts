aa769b9b98f3aa769edb89dbe22a44696e155774
meguito <doasilva2009@hotmail.com>
2026-08-26 20:59:21 -0300

    fix: repairing wrong users mock import and enabling usermocks export

--- src/service/mocks/registry.ts
+++ src/service/mocks/registry.ts
@@ -4,9 +4,9 @@ import type { User } from "@/domain/models/entities/user";
 
 import solarPanelAnnouncements from "./products/solarPanel.json";
 import companies from "./entities/company.json";
-import users from "./entities/company.json";
+import users from "./entities/users.json";
 
 export const solarPanelAnnouncementMocks =
   solarPanelAnnouncements as SolarPanelAnnouncement[];
 export const companyMocks = companies as Company[];
-// export const userMocks = users as User[];
+export const userMocks = users as User[];
