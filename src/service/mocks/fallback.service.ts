76142ed7f30fc28b70cba20d679a95451752725b
Davi Silva <doasilva2009@hotmail.com>
2026-08-26 15:56:13 -0300

    chore: establishing mocks architeture

--- src/service/mocks/fallback.service.ts
+++ src/service/mocks/fallback.service.ts
@@ -1,15 +1,24 @@
 import MocksMode from "@/domain/enum/mocksMode";
+import sleep from "@/utils/sleep";
 
 export async function resolveWithMocks<T>(apiCall: () => Promise<T>, mockCall: () => T | Promise<T>): Promise<T> {
     const mode = import.meta.env.VITE_MOCKS as MocksMode;
 
-    if (mode === MocksMode.Always) { return mockCall(); }
-
-    if (mode === MocksMode.Deactivated) { return apiCall(); }
-
+    if (mode === MocksMode.Always) {
+        await sleep(2000);
+        return mockCall();
+    }
+    
+    if (mode === MocksMode.Deactivated) {
+        return apiCall();
+    }
+    
     try {
         return await apiCall();
-    } catch {
+    }
+    
+    catch {
+        await sleep(2000);
         return mockCall();
     }
 }
