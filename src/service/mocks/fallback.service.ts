ed76fce822cb5f20066500cfbe546a57096df78a
Davi Silva <doasilva2009@hotmail.com>
2026-08-26 18:21:16 -0300

    refactor: uppercase for enum

--- src/service/mocks/fallback.service.ts
+++ src/service/mocks/fallback.service.ts
@@ -1,15 +1,22 @@
 import MocksMode from "@/domain/enum/mocksMode";
 import sleep from "@/utils/sleep";
+import logger from "@/config/logging/logger";
 
 export async function resolveWithMocks<T>(apiCall: () => Promise<T>, mockCall: () => T | Promise<T>): Promise<T> {
     const mode = import.meta.env.VITE_MOCKS as MocksMode;
+    const naptime = 2000
 
-    if (mode === MocksMode.Always) {
-        await sleep(2000);
+    async function takeNap() {
+        await sleep(naptime);
+        logger.info(`Mocks called before ${naptime} ms`)
+    }
+
+    if (mode === MocksMode.ALWAYS) {
+        takeNap()
         return mockCall();
     }
     
-    if (mode === MocksMode.Deactivated) {
+    if (mode === MocksMode.DEACTIVATED) {
         return apiCall();
     }
     
@@ -18,7 +25,7 @@ export async function resolveWithMocks<T>(apiCall: () => Promise<T>, mockCall: (
     }
     
     catch {
-        await sleep(2000);
+        takeNap()
         return mockCall();
     }
 }
