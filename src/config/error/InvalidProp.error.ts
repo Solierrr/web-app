d8f0d06c6b5eb51a4d329f6a791b9c7200e2b5c9
meguito <doasilva2009@hotmail.com>
2026-08-28 19:44:19 -0300

    chore: moving customized exceptions for config/

---
+++ src/config/error/InvalidProp.error.ts
@@ -0,0 +1,11 @@
+export class InvalidPropError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = "InvalidPropError";
+    }
+
+    static missingProps(component: string, props: string[]): InvalidPropError {
+        const propList = props.map((prop) => `\`${prop}\``).join(" ou ");
+        return new InvalidPropError(`${component}: informe ${propList}.`);
+    }
+}
