13b862a2fb2de8df4b3cc454be2906db5ea49c4b
meguito <doasilva2009@hotmail.com>
2026-08-31 00:15:47 -0300

    feat: add link ui component

---
+++ src/components/ui/link/Hyperlink.tsx
@@ -0,0 +1,23 @@
+import { Link, useLocation } from "react-router-dom";
+import { HyperlinkUrlType } from "./Hyperlink.enum";
+
+interface HyperlinkProps {
+    content: string;
+    url: string;
+    type?: HyperlinkUrlType;
+
+    className?: string;
+}
+
+export function Hyperlink({ content, url, type = HyperlinkUrlType.COMPLETE, className }: HyperlinkProps) {
+    let redirect: string;
+    if (type === HyperlinkUrlType.COMPLETE) { redirect = url }
+    else if (type === HyperlinkUrlType.CONCAT) { redirect = `${useLocation().pathname}/${url}` }
+    else { throw new Error() }
+
+    return (
+        <div className="flex w-fit px-1 bg-interactive rounded-small">
+            <Link className={`font-medium ${className}`} to={redirect}>{content}</Link>
+        </div>
+    );
+}
